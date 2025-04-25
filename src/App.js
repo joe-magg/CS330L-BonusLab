b328dd75df9e0efa7e896458bd6dae60069f235d
Joseph Maggio <56551598+joem23@users.noreply.github.com>
2025-04-25 15:59:16 -0400

    feat: add Bootstrap cards (3 side-by-side)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
+
 function App() {
   return (
     <div className="App">
@@ -11,6 +13,9 @@ function App() {
       <h2>Section - 010</h2>
       <p>WVU ID: 800418467</p>
       <p>Hi, I am Joe!</p>
+      <div classname="Card">
+        <Card />
+      </div>
     </div>
   );
 }
