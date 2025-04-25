dbddf6a742bf78a3059cae358371a8f74388fb0d
Joseph Maggio <56551598+joem23@users.noreply.github.com>
2025-04-25 16:47:12 -0400

    feat: add expandable navbar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-      <div classname="Navbar">
+      <div className="Navbar">
         <Navbar />
       </div>
       <h1>CS 330L</h1>
       <h2>Section - 010</h2>
       <p>WVU ID: 800418467</p>
       <p>Hi, I am Joe!</p>
+      <h3> These are the projects I am currently working on:</h3>
       <div classname="Card">
         <Card />
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
 function Navbar() {
-        return <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        </div>
-    </div>
-    </nav>
+    return (
+        <nav className="navbar bg-dark navbar-dark">
+            <div className="container-fluid d-flex align-items-center">
+                <div className="d-flex align-items-center">
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse" 
+                        data-bs-target="#collapsibleNavbar"
+                        aria-controls="collapsibleNavbar"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul className="navbar-nav d-flex flex-row">
+                        <li className="nav-item ms-3">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item ms-3">
+                            <a className="nav-link disabled" href="#" aria-disabled="true">Item2</a>
+                        </li>
+                        <li className="nav-item ms-3">
+                            <a className="nav-link disabled" href="#" aria-disabled="true">Item2</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
+
 export default Navbar;
