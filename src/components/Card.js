13cf68cb1b28aa9c521f595f579a87093cc187f5
Joseph Maggio <56551598+joem23@users.noreply.github.com>
2025-04-25 16:12:06 -0400

    feat: add Modal buttons inside of cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,31 @@
+import Modal from './Modal.js';
 
 function Card() {
     return (
-        <div class="card-group">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div className="card-group">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    
+                    < Modal />
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    < Modal />
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    < Modal />
                 </div>
             </div>
         </div> 
