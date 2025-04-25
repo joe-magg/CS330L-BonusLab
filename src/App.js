7ec3ffd346455e54da47af8af8692c5343417640
Joseph Maggio <56551598+joem23@users.noreply.github.com>
2025-04-25 15:04:50 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CS 330L</h1>
+      <h2>Section - 010</h2>
+      <p>WVU ID: 800418467</p>
+      <p>Hi, I am Joe!</p>
     </div>
   );
 }
